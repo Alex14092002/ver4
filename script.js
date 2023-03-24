c41a45f5b5a9daf1e1fa263dcbf21588b984084a
DEV PHU TUAN <2051010343tuan@ou.edu.vn>
2023-03-24 18:14:51 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+
+
+
+
+  var currentOverlay = document.querySelector('.overlay-black.overlay');
+  if (currentOverlay) {
+    currentOverlay.classList.remove('overlay');
+  }
+  var newOverlay = document.querySelectorAll('.overlay-black')[n-1];
+  newOverlay.classList.add('overlay');
+  // rest of the code
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+
+}
